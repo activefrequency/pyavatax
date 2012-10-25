afc19f3ce8c0f620af827956d74c37e314c8606f
John Obelenus <jobelenus@gmail.com>
2012-10-25 14:21:24 -0400

    project structure for packagin and distribution

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='PyAvaTax',
+    version='0.1dev',
+    packages=['pyavatax',],
+    license='BSD',
+    long_description=open('README.md').read(),
+)
+

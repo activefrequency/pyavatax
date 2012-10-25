6196d3ff518be5e9dd04c19347a1ef87b65e3313
John Obelenus <jobelenus@gmail.com>
2012-10-25 15:06:45 -0400

    ignore nuts and bolts, pypi setup complete

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from distutils.core import setup
+# python setup.py sdist bdist_wininst upload
 
 setup(
     name='PyAvaTax',
+    url = 'http://github.com/activefrequency/pyavatax/',
+    author = 'John Obelenus',
+    author_email = 'jobelenus@activefrequency.com',
     version='0.1dev',
+    install_requires = ['requests>=0.14.1', 'decorator>=3.4.0'],
+    package_data = {
+        '': ['*.txt', '*.rst', '*.md']
+    },
     packages=['pyavatax',],
     license='BSD',
     long_description=open('README.md').read(),

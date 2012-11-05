973d5067b91a9957eb16f5177b628ad633545d08
John Obelenus <jobelenus@gmail.com>
2012-11-05 14:38:59 -0500

    method for getting the recorder

---
+++ pyavatax/django_integration.py
@@ -0,0 +1,26 @@
+import datetime
+def get_django_recorder():
+    try:
+        import django
+        from pyavatax.models import AvaTaxRecord
+    except ImportError:
+        class MockDjangoRecorder(object):
+            
+            @staticmethod
+            def failure(doc, response):
+                pass
+
+            @staticmethod
+            def success(doc):
+                pass
+        return MockDjangoRecorder
+    else:
+        class RealDjangoRecorder(object):
+            @staticmethod
+            def failure(doc, response):
+                AvaTaxRecord.objects.create(doc_code=getattr(doc, 'DocCode', None), failure_details=response._details)
+
+            @staticmethod
+            def success(doc):
+                AvaTaxRecord.objects.filter(doc_code=getattr(doc, 'DocCode', None)).update(success_on=datetime.datetime.now())
+        return RealDjangoRecorder

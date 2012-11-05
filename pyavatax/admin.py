bca14d88a19fca4cdb207e617be8da6473939369
John Obelenus <jobelenus@gmail.com>
2012-11-05 14:39:37 -0500

    admin showing the recorder

---
+++ pyavatax/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from pyavatax import models
+
+
+class AvaTaxRecordAdmin(admin.ModelAdmin):
+    list_display = ['doc_code', 'logged_on', 'success_on' ]
+    ordering = ['logged_on', ]
+    search_fields = ['doc_code']
+
+admin.site.register(models.AvaTaxRecord, AvaTaxRecordAdmin)

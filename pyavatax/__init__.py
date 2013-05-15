ad302a77ca07a2a46be2367ba669e4ea362009a6
John Obelenus <jobelenus@gmail.com>
2013-05-15 18:15:16 -0400

    proper version file

---
+++ pyavatax/__init__.py
@@ -0,0 +1,4 @@
+VERSION = (1, 1)
+
+def get_version():
+    return ".".join([str(v) for v in VERSION])
--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 # python setup.py sdist
 # python setup.py sdist bdist_wininst upload
 
+version = __import__('pyavatax').get_version()
+
 setup(
     name='PyAvaTax',
     url = 'http://github.com/activefrequency/pyavatax/',
     author = 'John Obelenus',
     author_email = 'jobelenus@activefrequency.com',
-    version='1.0rc',
+    version=version,
     install_requires = ['requests==1.1', 'decorator==3.4.0', 'suds==0.4'],
     package_data = {
         '': ['*.txt', '*.rst', '*.md']
@@ -16,4 +18,3 @@ setup(
     license='BSD',
     long_description="PyAvaTax is a Python library for easily integrating Avalara's RESTful AvaTax API Service",
 )
-

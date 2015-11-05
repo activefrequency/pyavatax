7e340c61ca2c3cae4a7470d43cbaed49bfb8b1d3
John Obelenus <jobelenus@gmail.com>
2015-11-05 10:12:01 -0500

    proper __all__ definition for importing `pyavatax` as per the documentation suggests

--- pyavatax/__init__.py
+++ pyavatax/__init__.py
@@ -1,4 +1,12 @@
 VERSION = (1, 3, 1)
 
+
+from .api import API
+from .base import Document, Address, Line
+
+
+__all__ = ['API', 'Document', 'Address', 'Line', 'get_version']
+
+
 def get_version():
     return ".".join([str(v) for v in VERSION])
